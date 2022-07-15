ac1f7699582266b5bbe5ae4fdd44c243c8aca2d2
Dranic <78444980+ddranic@users.noreply.github.com>
2022-07-15 14:53:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import logging
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from pagination import InlinePagination
+
+token = "1515876167:AAF_Ql_1JFwaGcWxE7aoJHH2xHAoaXPrBRU"
+bot = Bot(token=token, parse_mode="HTML", disable_web_page_preview=True)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+logging.basicConfig(level=logging.INFO)
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    pagination = InlinePagination(button_datas=[(i, 2) for i in range(1, 100)], width=4)
+    kb = pagination.get_page_keyboard(cur_page=1)
+
+    await message.answer("пуперт нуб", reply_markup=kb)
+
+
+@dp.callback_query_handler(lambda c: c.data.startswith("n_"))
+async def next(call: types.CallbackQuery):
+    pagination = InlinePagination(button_datas=[(i, 2) for i in range(1, 100)], width=4)
+    kb = pagination.get_page_keyboard(cur_page=call.data)
+
+    await call.message.edit_reply_markup(reply_markup=kb)
+
+
+@dp.callback_query_handler(lambda c: c.data.startswith("b_"))
+async def back(call: types.CallbackQuery):
+    pagination = InlinePagination(button_datas=[(i, 2) for i in range(1, 100)], width=4)
+    kb = pagination.get_page_keyboard(cur_page=call.data)
+
+    await call.message.edit_reply_markup(reply_markup=kb)
+
+
+if __name__ == "__main__":
+    try:
+        executor.start_polling(dp, skip_updates=True)
+    except Exception as err:
+        print(err)
